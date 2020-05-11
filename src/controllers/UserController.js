bb8651a020a556e1f5f851879358d1cb0aa5ff95
Estevao <estevaovasques@hotmail.com>
2020-05-10 23:28:47 -0300

    CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,9 +3,49 @@ const knex = require('../database')
 
 module.exports = {
     async index(req, res) {
-
         const results = await knex('users')
 
         return res.json(results)
+    },
+
+    async create(req, res, next) {
+      try {
+        const { username } = req.body
+
+        await knex('users').insert({ username })
+
+        return res.sendStatus(201)
+      } catch (error) {
+        next(error)
+      }
+    },
+
+    async update(req, res, next) {
+        try {
+          const { username } = req.body
+          const { id } = req.params
+
+          await knex('users')
+            .update({ username })
+            .where({ id })
+
+          return res.sendStatus(200)
+        } catch (error) {
+          next(error)
+        }
+    },
+
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params
+
+            await knex('users')
+              .where({ id })
+              .del()
+
+            return res.sendStatus(204)
+          } catch (error) {
+            next(error)
+          }
     }
 }
--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 
+app.use(express.json())
 app.use(routes)
+
+
+//catch all
+
+app.use((error, req, res, next) => {
+    res.status(res.status || 500)
+
+    res.json({ error: error.message })
+})
 
 app.listen(3333,  () => console.log('Server is running'))
