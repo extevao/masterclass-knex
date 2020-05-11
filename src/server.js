4c0b86cea61e8566f1365b6d9a28fc2b43df5c82
Estevao <estevaovasques@hotmail.com>
2020-05-11 10:01:24 -0300

    Error handler que nao funciona

--- src/server.js
+++ src/server.js
@@ -5,9 +5,15 @@ const routes = require('./routes')
 app.use(express.json())
 app.use(routes)
 
+//noFound
+// app.use((req, res, next) => {
+//     const error = new Error('Not found')
+//     error.status = 404
+
+//     next(error)
+// })
 
 //catch all
-
 app.use((error, req, res, next) => {
     res.status(res.status || 500)
 
